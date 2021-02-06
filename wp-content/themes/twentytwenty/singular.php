b2f90b5e170e937f968ca0eb2d03601a8623dda0
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2021-02-06 14:09:46 +0100

    fixes

--- wp-content/themes/twentytwenty/singular.php
+++ wp-content/themes/twentytwenty/singular.php
@@ -138,7 +138,7 @@ get_header();
                 <?= z_taxonomy_image($cat->term_id); ?>
                 <div class="category-hovered-box d-flex flex-column position-absolute top-0 start-0 w-100 h-100 p-3 pe-0">
                   <h3 class="mb-3 text-white text-uppercase"><?= $cat->cat_name; ?></h3>
-                  <p class="text-white"><?= $cat->description; ?></p>
+                  <p class="text-white pe-2"><?= $cat->description; ?></p>
                   <span class="mt-auto ms-auto text-white text-uppercase me-2">Zobacz wiecej</span>
                 </div>
               </a>
